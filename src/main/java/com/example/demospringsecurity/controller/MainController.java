e8058ffb59e55f6cec8be47ad17ce62a10d6afd7
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-24 00:06:53 +0700

    refactor

--- src/main/java/com/example/demospringsecurity/controller/MainController.java
+++ src/main/java/com/example/demospringsecurity/controller/MainController.java
@@ -1,16 +1,12 @@
 package com.example.demospringsecurity.controller;
 
-import com.example.demospringsecurity.model.Accounts;
-import com.example.demospringsecurity.util.WebUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.util.WebUtils;
 
 import java.security.Principal;
 
--- src/main/java/com/example/demospringsecurity/util/EncryptedPasswordUtil.java
+++ src/main/java/com/example/demospringsecurity/util/EncryptedPasswordUtil.java
@@ -3,14 +3,14 @@ package com.example.demospringsecurity.util;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class EncryptedPasswordUtil {
-    public static String encrytePassword(String password) {
+    public static String encryptPassword(String password) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder.encode(password);
     }
 
     public static void main(String[] args) {
         String password = "123";
-        String encrytedPassword = encrytePassword(password);
+        String encrytedPassword = encryptPassword(password);
 
         System.out.println("Encryted Password: " + encrytedPassword);
     }
